5838c1b85a7f6757e053d30e1e92a04c1b0a854e
laik <laik.lj@me.com>
2023-02-06 10:16:23 +0800

    xxx

--- src/service/settings/shop.tsx
+++ src/service/settings/shop.tsx
@@ -183,15 +183,15 @@ const defaultFrom: FormProps = {
     onSubmit: (formRef, values, handleClose) => {
         let shop: Partial<Shop> = {
             name: values.name,
-            mode: values.mode == "1" ? 1 : 0,
+            mode: values.mode === "1" ? 1 : 0,
             address: values.address,
             logo: values.logo?.fileList[0].name,
             industry: values.industry,
             introduction: values.introduction,
-            recommend_door: values.recommend_door == "1",
+            recommend_door: values.recommend_door === "1",
         };
 
-        if (values.recommend_door_name != "" || values.recommend_door_name != undefined) {
+        if (values.recommend_door_name !== "" || values.recommend_door_name !== undefined) {
             shop.recommend_door_name = values.recommend_door_name
         }
         shopApi.update(shop).
