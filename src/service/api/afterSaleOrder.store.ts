0ed7a56b1c6732f730ac4e0464539613ed34c42d
YoshiAki <yoshiakiley@gmail.com>
2023-03-01 09:53:50 +0800

    add after-sale

--- src/service/api/afterSaleOrder.store.ts
+++ src/service/api/afterSaleOrder.store.ts
@@ -1,5 +1,6 @@
 import { IObject, ObjectApi, ObjectStore } from '@/client';
 import { DefaultWatchApi, WatchApi } from '@/client/event';
+import { MerchandiseList } from './order.store';
 
 
 export class AfterSaleOrder extends IObject {
@@ -15,10 +16,32 @@ export class AfterSaleOrder extends IObject {
   deliveries: any | undefined
   // 
 
+  merchandise_list: MerchandiseList[] | undefined
+  total_render: number | undefined
+
   constructor(data: AfterSaleOrder) {
     super(data);
     Object.assign(this, data);
 
+
+    this.total_render = this.total ? this.total / 100 : 0
+    this.merchandise_list = this.order_sku_list?.map(
+      (item: any) => {
+        let attrs: any = {}
+        attrs['数量'] = item.quantity
+        attrs['价格'] = item.amount
+
+        if (item.sku.spec_name) {
+          attrs['规格'] = item.sku.spec_name
+        }
+        return {
+          image: '/media-t/file/' + item.sku?.pic || '',
+          title: item.sku?.product_name,
+          uid: item.sku?.uid,
+          attrs: attrs
+        }
+      }
+    )
   }
 }
 
--- src/service/order/after-sale-order/page.tsx
+++ src/service/order/after-sale-order/page.tsx
@@ -1,13 +1,12 @@
 import { StoreTableProps } from '@/dynamic-components';
 import { MenuButtonType } from '@/dynamic-components/table/menuButton';
 import { pageManager } from '@/dynamic-view';
-import { Order, orderApi, orderStore, afterSaleOrderStore } from '@/service/api';
+import { Order, afterSaleOrderStore } from '@/service/api';
 import { merge } from 'lodash';
-import { shipForm } from './form';
 
-const orderStoreTable: StoreTableProps = {
+const afterSaleOrderStoreTable: StoreTableProps = {
   rowKey: 'uid',
-  store: orderStore,
+  store: afterSaleOrderStore,
   search: false,
   size: 'small',
   columns: [
@@ -27,28 +26,27 @@ const orderStoreTable: StoreTableProps = {
       editable: false,
     },
     {
-      dataIndex: 'total_render',
-      title: '实收金额',
+      dataIndex: 'total',
+      title: '应退金额',
       valueType: 'money',
       editable: false,
-
     },
     {
       dataIndex: 'customer',
       title: '客户信息',
       editable: false,
     },
-    {
-      dataIndex: 'delivery_type',
-      title: '配送方式',
-      hideInSearch: true,
-      editable: false,
-    },
-    {
-      dataIndex: 'payment_type',
-      title: '支付方式',
-      editable: false,
-    },
+    // {
+    //   dataIndex: 'delivery_type',
+    //   title: '配送方式',
+    //   hideInSearch: true,
+    //   editable: false,
+    // },
+    // {
+    //   dataIndex: 'payment_type',
+    //   title: '支付方式',
+    //   editable: false,
+    // },
     {
       dataIndex: 'typ',
       title: '订单状态',
@@ -63,72 +61,47 @@ const orderStoreTable: StoreTableProps = {
 
   toolbarTitle: '订单列表',
   tableMenu: (record: Order, action) => {
-    let columns: MenuButtonType[] = [{
-      kind: 'link',
-      title: '详情',
-      link: `/order/order/detail?uid=${record.uid}`,
-    },
-    {
-      kind: 'implement',
-      title: '刷新',
-      onClick(e) {
-        action?.reload()
+    let columns: MenuButtonType[] = [
+      {
+        kind: 'link',
+        title: '详情',
+        link: `/order/after-sale-order/detail?uid=${record.uid}`,
       },
-    }]
-    columns.push({
-      kind: 'form',
-      title: '发货',
-      onMount: async ({ form, setDataObject, }) => {
-        let data = await orderApi.get(record.uid).then((res) => {
-          let delivery_map = new Map();
-
-          (res?.order_sku_list || []).map((item: any) => {
-            delivery_map.set(item.sku.uid, item.quantity)
-          })
-          res.deliveries?.map(
-            (item: any) => {
-              let quantity = delivery_map.get(item.sku_id) - item.quantity
-              delivery_map.set(item.sku_id, quantity)
-            }
-          )
-          res.delivery_map = delivery_map
-          setDataObject(res)
-          return res;
-        })
-
-        let dataSource: any[] = [];
-        record.merchandise_list?.map(item => {
-          if (record.delivery_map?.get(item.uid) == 0) {
-            return
-          }
-          dataSource.push({
-            uid: item.uid,
-            merchandise: item,
-            number1: record.delivery_map?.get(item.uid),
-            number2: record.delivery_map?.get(item.uid)
-          })
-        })
-
-        form?.setFieldsValue({
-          merchandiseTable: {
-            dataSource: dataSource,
-          },
-          orderId: record._id
-        });
+      {
+        kind: 'implement',
+        title: '刷新',
+        onClick(e) {
+          action?.reload()
+        },
       },
-      ...shipForm,
-    })
+      {
+        kind: 'implement',
+        collapse: true,
+
+        title: '通过',
+        onClick(e) {
+        },
+      },
+      {
+        kind: 'implement',
+        title: '拒绝',
+        collapse: true,
+
+        onClick(e) {
+        },
+      },
+    ]
     return columns
   },
   onRequest: (params) => {
     const query = merge(params, { sort: { version: 1 } });
-    orderStore.next(query)
+    afterSaleOrderStore.next(query)
   },
 };
 
 pageManager.register('order.after-sale-order', {
   page: {
-    view: [{ kind: 'storeTable', ...orderStoreTable }],
+    view: [{ kind: 'storeTable', ...afterSaleOrderStoreTable }],
     container: {
       keepAlive: false,
     },
