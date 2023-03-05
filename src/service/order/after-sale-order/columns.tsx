a1f21a58b48d2fabd28e70a30deca39a0e52fe1c
YoshiAki <yoshiakiley@gmail.com>
2023-03-05 21:45:08 +0800

    update

---
+++ src/service/order/after-sale-order/columns.tsx
@@ -0,0 +1,20 @@
+import { FormColumnsType } from '@/dynamic-components';
+
+
+
+export const orderId: FormColumnsType = {
+    dataIndex: 'orderId',
+    title: '订单id',
+    fieldProps: {
+        disabled: true
+    }
+}
+
+export const orderTime: FormColumnsType = {
+    dataIndex: 'orderTime',
+    title: '下单时间',
+    fieldProps: {
+        disabled: true
+    }
+}
+
