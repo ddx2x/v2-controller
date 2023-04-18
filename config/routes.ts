9131525147e80bf5fb9ef206d78d61e3d6d4d71a
YoshiAki <yoshiakiley@gmail.com>
2023-04-18 16:24:47 +0800

    add

--- config/routes.ts
+++ config/routes.ts
@@ -216,6 +216,20 @@ export default [
       },
     ],
   },
+  {
+    name: '营销',
+    path: '/marketing',
+    icon: 'shop',
+    routes: [
+      {
+        name: '折扣活动',
+        path: '/marketing/activity',
+        component: './dynamic-view',
+        icon: 'mehTwoTone',
+        routes: [],
+      },
+    ],
+  },
 
   {
     name: '店铺',
@@ -365,7 +379,7 @@ export default [
             parentKeys: ['/setting/delivery'],
             hideInMenu: true,
           },
-          
+
           {
             name: '新增自提点',
             path: '/setting/delivery/storepickupadd',
--- src/service/index.ts
+++ src/service/index.ts
@@ -5,3 +5,4 @@ export * from './order';
 export * from './privilege';
 export * from './product';
 export * from './settings';
+export * from './marketing';
